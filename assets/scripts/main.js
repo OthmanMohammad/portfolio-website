b72c3627fbd620f71393f413cce29a2d84c2500e
Mohammad Othman <emranm226@hotmail.com>
2023-04-02 00:47:44 +0100

    feat: Enhance vertical navbar with active classes and improved styles
    Changes include:
    - Increase the length of the vertical bar
    - Add active class styling for nav items
    - Implement new styles for smoother glowing effect around nav items
    - Add animated gradient line and moving triangle for active nav items

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,21 @@
+function setActiveLink() {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach((link) => {
+        if (link.pathname === currentPath) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setActiveLink();
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const lottieIcons = document.querySelectorAll(".lottie-icon");
   
