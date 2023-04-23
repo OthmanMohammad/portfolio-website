98687f44e6c2f7fa79ff16efecbeaf5bb3423480
Mohammad Othman <emranm226@hotmail.com>
2023-04-23 18:17:58 +0100

    implemented a moving rocket in background of index

---
+++ assets/scripts/trace.js
@@ -0,0 +1,32 @@
+const rocket = document.querySelector(".rocket");
+
+function moveRocket() {
+    const containerRect = document.body.getBoundingClientRect();
+    const rocketRect = rocket.getBoundingClientRect();
+
+    const startX = rocket.offsetLeft;
+    const startY = rocket.offsetTop;
+
+    const targetX = Math.random() * (containerRect.width - rocketRect.width);
+    const targetY = Math.random() * (containerRect.height - rocketRect.height);
+
+    const controlX = (startX + targetX) / 2;
+    const controlY = (startY + targetY) / 2;
+
+    const animation = rocket.animate(
+        [
+            { transform: `translate(${startX}px, ${startY}px)` },
+            { transform: `translate(${controlX}px, ${controlY}px)` },
+            { transform: `translate(${targetX}px, ${targetY}px)` },
+        ],
+        {
+            duration: 5000, // Increase or decrease the duration for each movement in milliseconds
+            easing: "linear",
+            fill: "forwards",
+        }
+    );
+
+    animation.onfinish = moveRocket;
+}
+
+moveRocket();
