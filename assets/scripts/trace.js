d80cf8d96375743bacdc3736b28df7c061ba4255
Mohammad Othman <emranm226@hotmail.com>
2023-04-27 06:13:27 +0100

    Apply code formatting and cleanup for HTML and CSS files

--- assets/scripts/trace.js
+++ assets/scripts/trace.js
@@ -1,36 +1,35 @@
 const rocket = document.querySelector(".rocket");
 
 function moveRocket() {
-    const containerRect = document.body.getBoundingClientRect();
-    const rocketRect = rocket.getBoundingClientRect();
+  const containerRect = document.body.getBoundingClientRect();
+  const rocketRect = rocket.getBoundingClientRect();
 
-    const startX = rocket.offsetLeft;
-    const startY = rocket.offsetTop;
+  const startX = rocket.offsetLeft;
+  const startY = rocket.offsetTop;
 
-    const targetX = Math.random() * (containerRect.width - rocketRect.width);
-    const targetY = Math.random() * (containerRect.height - rocketRect.height);
+  const targetX = Math.random() * (containerRect.width - rocketRect.width);
+  const targetY = Math.random() * (containerRect.height - rocketRect.height);
 
-    const controlX = (startX + targetX) / 2;
-    const controlY = (startY + targetY) / 2;
+  const controlX = (startX + targetX) / 2;
+  const controlY = (startY + targetY) / 2;
 
-    const animation = rocket.animate(
-        [
-            { transform: `translate(${startX}px, ${startY}px)` },
-            { transform: `translate(${controlX}px, ${controlY}px)` },
-            { transform: `translate(${targetX}px, ${targetY}px)` },
-        ],
-        {
-            duration: 5000, // Increase or decrease the duration for each movement in milliseconds
-            easing: "linear",
-            fill: "forwards",
-        }
-    );
+  const animation = rocket.animate(
+    [
+      { transform: `translate(${startX}px, ${startY}px)` },
+      { transform: `translate(${controlX}px, ${controlY}px)` },
+      { transform: `translate(${targetX}px, ${targetY}px)` },
+    ],
+    {
+      duration: 5000, // Increase or decrease the duration for each movement in milliseconds
+      easing: "linear",
+      fill: "forwards",
+    }
+  );
 
-    animation.onfinish = moveRocket;
+  animation.onfinish = moveRocket;
 }
 
 // trace.js
 if (rocket) {
-    moveRocket();
-  }
-  
+  moveRocket();
+}
